28a2b0ee050aaace2463a756baa8c9a097e3ee8c
Ayşe nur Özselçuk <ozselcukaysenur@gmail.com>
2024-11-27 13:02:30 +0300

    user
    user uygulaması yapıldı, blog ile bağlantısı sağlandı, login,create html dosyaları yazıldı

---
+++ user/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def register(request):
+    return render(request,"register.html")
+def loginuser(request):
+    return render(request,"login.html")
+def logoutuser(request):
+    pass
