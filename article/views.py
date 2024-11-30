f525acb7edb5a94ada16f9421f540c737d4aded6
Ayşe nur Özselçuk <ozselcukaysenur@gmail.com>
2024-11-30 15:15:46 +0300

    addarticle
    addarticle.html olutşruruldu, forms oluşturuldu, modelsten aktarılacak form olutşuruldu ve viewsta sayfada gösterildi

--- article/views.py
+++ article/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render,HttpResponse
-
+from .forms import ArticleForm
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
 def about(request):
     return render(request,"about.html")
+
+def dashboard(request):
+    return render(request,"dashboard.html")
+
+def addArticle(request):
+    form = ArticleForm()
+
+    return render(request, "addarticle.html",{"form":form})
