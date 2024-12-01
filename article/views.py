c9c1109fc4476e8cec4f19286d5299fae496b61a
Ayşe nur Özselçuk <ozselcukaysenur@gmail.com>
2024-12-01 14:42:21 +0300

    article sayfasını veri tabanına kaydetme
    article uygulamasına addarticle views ı eklendi

--- article/views.py
+++ article/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from .forms import ArticleForm
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request,"index.html")
@@ -11,6 +12,15 @@ def dashboard(request):
     return render(request,"dashboard.html")
 
 def addArticle(request):
-    form = ArticleForm()
+    form = ArticleForm(request.POST or None)
 
+    if form.is_valid():
+        article= form.save(commit=False)
+
+        article.author = request.user
+        article.save()
+
+        messages.success(request,"Makale Başarıyla Oluşturuldu")
+        return redirect("index")
+    
     return render(request, "addarticle.html",{"form":form})
