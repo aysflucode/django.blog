08fafb38de7a5678f96dddf8cea30e87a4239d0c
Ayşe nur Özselçuk <ozselcukaysenur@gmail.com>
2024-12-03 15:13:27 +0300

    Makale güncelleme ve silme
    makale silme güncelleme özellikleri butonlara aktarıldı

--- article/views.py
+++ article/views.py
@@ -26,7 +26,7 @@ def addArticle(request):
         article.save()
 
         messages.success(request,"Makale Başarıyla Oluşturuldu")
-        return redirect("index")
+        return redirect("article:dashboard")
     
     return render(request, "addarticle.html",{"form":form})
 
@@ -35,3 +35,25 @@ def detail(request,id):
     article =get_object_or_404(Article,id= id)
     return render(request, "detail.html",{"article":article})
 
+def updateArticle(request,id):
+    
+    article = get_object_or_404(Article, id=id)
+    form = ArticleForm(request.POST or None, request.FILES or None, instance = article)
+
+    if form.is_valid():
+        article= form.save(commit=False)
+        article.author = request.user
+        article.save()
+
+        messages.success(request, "Makale Başarıyla Güncellendi")
+        return redirect("article:dashboard")
+
+
+    return render(request,"update.html",{"form":form})
+
+def deleteArticle(request,id):
+    article = get_object_or_404(Article, id=id)
+    article.delete()
+
+    messages.success(request, "Makale Başarıyla Silindi")
+    return redirect("article:dashboard")
